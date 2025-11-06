952bc301c6f45ee3915c3c17c38d18262d5d5050
casey walker <csy_walker@yahoo.com>
2025-11-06 16:48:44 -0500

    fix: pick first public subnet from map for EC2

--- infra/terraform/ec2.tf
+++ infra/terraform/ec2.tf
@@ -2,15 +2,19 @@
 # EC2 instances: Mongo (intentionally weak) + Jenkins
 ############################################################
 
-# MongoDB EC2
+# pick the first public subnet from the for_each map
+locals {
+  first_public_subnet_id = values(aws_subnet.public)[0].id
+}
+
 resource "aws_instance" "mongo" {
   ami                         = data.aws_ami.ubuntu_2004.id
   instance_type               = "t3.micro"
-  subnet_id                   = aws_subnet.public[0].id
+  subnet_id                   = local.first_public_subnet_id
   vpc_security_group_ids      = [aws_security_group.mongo.id]
   associate_public_ip_address = true
 
-  # use existing instance profile (now defined in iam.tf as data ...)
+  # existing instance profile from iam.tf (data source)
   iam_instance_profile = data.aws_iam_instance_profile.mongo.name
 
   tags = {
@@ -22,15 +26,14 @@ resource "aws_instance" "mongo" {
   }
 }
 
-# Jenkins EC2
 resource "aws_instance" "jenkins" {
   ami                         = data.aws_ami.ubuntu_2004.id
   instance_type               = "t3.medium"
-  subnet_id                   = aws_subnet.public[0].id
+  subnet_id                   = local.first_public_subnet_id
   vpc_security_group_ids      = [aws_security_group.jenkins.id]
   associate_public_ip_address = true
 
-  # use existing instance profile (now defined in iam.tf as data ...)
+  # existing instance profile from iam.tf (data source)
   iam_instance_profile = data.aws_iam_instance_profile.jenkins.name
 
   tags = {
